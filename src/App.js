f0ba50bbcb576f58cd7f7f796679bf31041f1f40
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-11-29 17:11:03 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import FeatureTwo from './components/FeatureTwo';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import VideoSection from './components/VideoSection';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <>
-   <div className="flex flex-col items-center justify-center">
+   <div className="flex flex-col items-center justify-center  pt-[60px]">
       <Header/>
+      <Slider/>
       <FeatureOne/>
       <FeatureTwo/>
       <VideoSection/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import aos from "../assets/AndroidOS.png";
 import ios from "../assets/AppleLogo.png";
+import logo from "../assets/Herd-Help-Logo.png";
 
 const Header = () => {
   return (
-    <nav className='flex w-full h-[60px] bg-black text-white justify-between items-center px-24 '>
-        <div className='flex '>
-            <h3>Herd Help</h3>
+    <nav className='flex w-full h-[60px] bg-black text-white justify-between items-center px-24 fixed z-30 top-0 '>
+        <div className='flex justify-start items-center '>
+            {/* <h3>Herd Help</h3> */}
+            <img src={logo} alt="" className=' w-[41%] invert'  />
         </div>
         <div className="flex w-fit gap-5 ">
             <img src={aos} alt="" className='w-[30px] h-[30px]'/>
