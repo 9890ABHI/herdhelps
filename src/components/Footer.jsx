147acbe0eefd932abdcc1e4283d7718da3375d98
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-11-29 16:14:18 +0530

    completed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,56 @@ const Footer = () => {
     <div className='relative flex w-full '>
         <img src={bg} alt="" className='absolute z-0 inset-0 w-full' />
         <div className="flex flex-col w-full z-10 justify-center items-center py-16">
-        <div className="text-lg font-bold text-[#39B54A] ">THE ONE APP</div>
-            <div className="text-4xl font-semibold text-center text-white">That puts you back in contol </div>
-            <div className="flex justify-center items-center py-10">
-                <div className="bg-white w-[60%] h-28 py-10 px-10">
-                    <h5 className='text-black text-lg font-bold '>SEND US A MESSAGE</h5>
-                    <form action=""> 
-                    <TextField id="standard-basic" label="Standard" variant="standard" />
+        <div className="text-lg font-bold text-[#39B54A] pb-5 ">CONTACT US</div>
+            <div className="text-4xl font-semibold text-center text-white pb-10">Reach out for help or just <br /> say hello </div>
+            <div className="flex justify-start items-start w-[70%] py-10 ">
+                <div className="flex flex-col bg-black bg-opacity-50 w-[60%]  py-16 px-10">
+                    <h5 className='text-white text-lg font-bold pb-10'>SEND US A MESSAGE</h5>
+                    <form action="" className='flex flex-col gap-10 text-white'> 
+                    <TextField  sx={{
+                      // borderBottom: '1px solid #eeeeee70',
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:before':{  borderBottom: '2px solid #eeeeee70'    },
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:after':{  borderBottom: '2px solid #39B54A'    }
+                      }} 
+                     InputLabelProps={{ style: { color: '#eeeeee70' },  }}  color='success' label="Your Name" variant="standard"  />
+                    <TextField sx={{
+                      // borderBottom: '1px solid #eeeeee70',
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:before':{  borderBottom: '2px solid #eeeeee70'    },
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:after':{  borderBottom: '2px solid #39B54A'    }
+                      }}   InputLabelProps={{ style: { color: '#eeeeee70' },  }} color='success' label="Your Email" variant="standard" />
+                    <TextField sx={{
+                      // borderBottom: '1px solid #eeeeee70',
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:before':{  borderBottom: '2px solid #eeeeee70'    },
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:after':{  borderBottom: '2px solid #39B54A'    }
+                      }}   InputLabelProps={{ style: { color: '#eeeeee70' },  }} color='success' label="Subject" variant="standard" />
+                    <TextField  sx={{
+                      // borderBottom: '1px solid #eeeeee70',
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:before':{  borderBottom: '2px solid #eeeeee70'    },
+                    '.css-pjaces-MuiInputBase-root-MuiInput-root:after':{  borderBottom: '2px solid #39B54A'    }
+                      }}  InputLabelProps={{ style: { color: '#eeeeee70' },  }} color='success' label="Your Message" variant="standard" multiline rows={5} />
+
                     </form>
                 </div>
-                <div className="bg-[#222222] w-[200px]"></div>
+
+                <div className="bg-[#222222] w-[40%] pt-16 pb-[52px] px-10">
+                <h5 className='text-white text-lg font-bold '>CONTACT INFO</h5>
+
+                  <div className="flex flex-col py-5 gap-5">                    
+                <h5 className='text-[#39B54A] text-lg font-bold '>Where to Find Us</h5>
+                <p className='text-[#eeeeee70] text-base font-medium'>1600 Amphitheatre <br /> ParkwayMountain <br /> View, CA94043 US</p>
+                  </div>
+
+                <div className="flex flex-col py-5 gap-5">                    
+                <h5 className='text-[#39B54A] text-lg font-bold '>Email Us At</h5>
+                <p className='text-[#eeeeee70] text-base font-medium'>contact@herdhelp.com <br /> info@herdhelp.com</p>
+                    </div>
+
+                  <div className="flex flex-col py-5 gap-5">
+                <h5 className='text-[#39B54A] text-lg font-bold '>Call Us At</h5>
+                <p className='text-[#eeeeee70] text-base font-medium'>Phone: (+63) 555 1212 <br /> Mobile: (+63) 555 0100 <br /> Fax: (+63) 555 0101</p>
+                  </div>
+
+                </div>
             </div>
         </div>
     </div>
